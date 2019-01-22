d9d39e00fefc65e5e40dd6bf81f56db3da770436
luissoldevilla <luis.angelsoto@hotmail.com>
2019-01-21 19:51:50 -0500

    - Installed Styled components - Work on reusable components:   - Button Component   - Tile Component - Styled the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
-
-
+import styled from 'styled-components'
+import { ButtonContainer } from './Button'
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       {/*
       https://www.iconfinder.com/icons/1243689/call_phone_icon
       Creative Commons (Attribution 3.0 Unported);
@@ -22,12 +22,24 @@ export default class Navbar extends Component {
         </li>
       </ul>
       <Link to='/cart' className="ml-auto">
-        <buttom>
-          <i className="fas fa-cart-plus" />
+        <ButtonContainer>
+          <span className="mr-2">
+            <i className="fas fa-cart-plus" />
+          </span>
           my cart
-        </buttom>
+        </ButtonContainer>
       </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: nav(--mainWhite)!important;
+    font-size: 1.3rem;
+    /* 1rem 16px */
+    text-transform: capitalize;
+  }
+`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title'
 export default class ProductList extends Component {
+  state={
+    product: []
+  }
   render() {
     return (
-      <div>
-        <Product />
+      <React.Fragment>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="products" />
+
+          <div className="row" />
+
+        </div>
       </div>
+      </React.Fragment>
+      // <Product />
     )
   }
 }
