8bd53a4a3490d254b3b38b970f10bc28aae315cb
luissoldevilla <luis.angelsoto@hotmail.com>
2019-02-08 12:15:31 -0500

    Started working on the Checkout part of the app   -Cart folder was created in order to store all of the components   of the Cart   -package.json was implemented in order to maintain separation of   concerns   -Empty Cart components was created and implemented and tested

--- src/context.js
+++ src/context.js
@@ -9,9 +9,12 @@ class ProductProvider  extends Component {
   state ={
     products:[],
     detailProduct:detailProduct,
-    cart:[],
+    cart: storeProducts,
     modalOpen: false,
     modalProduct:detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   }
 
   componentDidMount() {
@@ -70,6 +73,23 @@ class ProductProvider  extends Component {
       return {modalOpen:false}
     })
   }
+
+  increment = (id) => {
+    console.log('this is increment method');
+  }
+
+  decrement = (id) => {
+    console.log('this is decrement method');
+  }
+
+  removeItem = (id) => {
+    console.log('item removed')
+  }
+
+  clearCart = () => {
+    console.log('cart was cleared')
+  }
+
   render() {
     return (
       <ProductContext.Provider value={{
@@ -77,7 +97,11 @@ class ProductProvider  extends Component {
         handleDetail:this.handleDetail,
         addToCart:this.addToCart,
         openModal:this.openModal,
-        closeModal:this.closeModal
+        closeModal:this.closeModal,
+        increment:this.increment,
+        decrement:this.decrement,
+        removeItem:this.removeItem,
+        clearCart:this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
